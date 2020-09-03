34fde16e36adf8fa4248d16b79064ccbf2e526ec
Heitor Souza <heitorhenriquedias@gmail.com>
2020-09-03 06:42:51 -0300

    064503092020

--- Chat/app.js
+++ Chat/app.js
@@ -1,7 +1,21 @@
 let app = require('./config/server');
 
-app.listen(8000, () => {
+let port = app.listen(8000, () => {
 
     console.log('\x1b[32m%s\x1b[0m', 'Server running');
+
+});
+
+let io = require('socket.io').listen(port);
+
+io.on('connection', (socket) => {
+
+    console.log('User connected');
+
+    socket.on('disconnect', () => {
+
+        console.log('\x1b[36m', 'Good bye user');
+
+    });
 
 });
